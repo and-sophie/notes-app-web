f8ee5d763d853a6bfec6dec3babedb4491669c53
Anna Lucking <alucking@and.digital>
2017-08-01 15:04:20 +0100

    :gear: configured webpack and set up basic react app

---
+++ src/components/hello.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// export default () => (
+//   <h1>Hello, world!</h1>
+// )
+
+class Hello extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+      </div>
+    )
+  }
+}
+
+export default Hello;
