55cee36f4221d5fbf49944bed76fafe36076f4db
Sophie Gill <sgill@andigital.com>
2017-08-02 12:27:57 +0100

    :gear: configures linter to use airbnb standard

--- src/components/hello.jsx
+++ src/components/hello.jsx
@@ -1,13 +1,5 @@
 import React from 'react';
 
-// export default () => (
-//   <h1>Hello, world!</h1>
-// )
-
-class Hello extends React.Component {
-  render() {
-    return <h1>Hello, world!</h1>
-  }
-}
-
-export default Hello
+export default () => (
+  <h1>Hello, world!</h1>
+);
